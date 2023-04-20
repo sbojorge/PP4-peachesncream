7b131cf9cda93061291324efaa0d4ad00f3add8c
sbojorge <sara.bojorge@gmail.com>
2023-04-20 17:59:00 +0000

    add create_list.html template

--- grocery_list/urls.py
+++ grocery_list/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import LandingPage
+# from .views import LandingPage
+from .views import CreateList
 
 urlpatterns = [
-    path('', LandingPage.as_view(), name='home')
+    # path('', LandingPage.as_view(), name='home'),
+    path('', CreateList.as_view(), name='create_list')
 ]
--- grocery_list/views.py
+++ grocery_list/views.py
@@ -1,5 +1,9 @@
 from django.views import generic
 
 
-class LandingPage(generic.TemplateView):
-    template_name = 'grocery_list/home.html'
+# class LandingPage(generic.TemplateView):
+#     template_name = 'grocery_list/home.html'
+
+
+class CreateList(generic.TemplateView):
+    template_name = 'grocery_list/create_list.html'
