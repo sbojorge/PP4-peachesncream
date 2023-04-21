71950bdee774726e950d0ff6293b49d8e79711c0
sbojorge <sara.bojorge@gmail.com>
2023-04-21 13:52:12 +0000

    update landing page url

--- grocery_list/views.py
+++ grocery_list/views.py
@@ -1,11 +1,11 @@
 from django.views import generic
-from .models import Grocery
+# from .models import Grocery
 
 
-# class LandingPage(generic.TemplateView):
-#     template_name = 'grocery_list/home.html'
+class LandingPage(generic.TemplateView):
+    template_name = 'grocery_list/home.html'
 
 
-class CreateList(generic.ListView):
-    model = Grocery
-    template_name = 'grocery_list/create_list.html'
+# class CreateList(generic.ListView):
+#     model = Grocery
+#     template_name = 'grocery_list/create_list.html'
