44964b3d6ea94cc139d4d032df32eea0a5d4e5fd
sbojorge <sara.bojorge@gmail.com>
2023-04-24 05:51:24 +0000

    Create the Grocery model

--- grocery_list/migrations/0001_initial.py
+++ grocery_list/migrations/0001_initial.py
@@ -14,8 +14,17 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Grocery',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(default='My grocery list', max_length=100)),
+                (
+                    'id',
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name='ID',
+                    )
+                ),
+                ('name', models.CharField(default='My grocery list',
+                 max_length=100)),
                 ('item', models.CharField(max_length=100)),
             ],
         ),
--- grocery_list/models.py
+++ grocery_list/models.py
@@ -1,2 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+
+class Grocery(models.Model):
+    """
+    Create a grocery list
+    """
+    name = models.CharField(max_length=100, default='My grocery list')
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name='grocery_owner')
+    created_on = models.DateTimeField(auto_now_add=True)
+    item = models.CharField(max_length=300, null=False, blank=False)
+
+    class Meta:
+        ordering: ['-created_on']
+
+    def __str__(self):
+        return self.name
