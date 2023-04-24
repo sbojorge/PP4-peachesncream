d5738b4d33b698b109ea19ab1544628c1baa1109
sbojorge <sara.bojorge@gmail.com>
2023-04-24 08:54:36 +0000

    Update create_list.html template

--- grocery_list/admin.py
+++ grocery_list/admin.py
@@ -1,1 +1,13 @@
 from django.contrib import admin
+from .models import Grocery
+
+
+@admin.register(Grocery)
+class GroceryAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'author',
+        'created_on'
+    )
+
+    list_filter = ('author', 'created_on')
--- grocery_list/views.py
+++ grocery_list/views.py
@@ -1,7 +1,16 @@
 from django.views import generic
-
+from .models import Grocery 
 
 
 class LandingPage(generic.TemplateView):
+    """
+    Render the landing page to the UI
+    """
     template_name = 'grocery_list/home.html'
+
+class AddItem(generic.CreateView):
+    """
+    Add grocery item to the shopping list
+    """
+    
 
