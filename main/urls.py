8f6b8a3fceaac3c5cc4745e6e0759730cba9af16
sbojorge <sara.bojorge@gmail.com>
2023-04-25 14:12:43 +0000

    rename and reorganize project and its folders and files

--- main/urls.py
+++ main/urls.py
@@ -1,4 +1,4 @@
-"""shopping_list URL Configuration
+"""main URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
